a179187cac1d15fc7078b5a3c53b6e002359168e
naveenc1x <naveenkumar.viswanathan@c1exchange.com>
2023-11-06 18:24:21 +0530

    fiber

--- app.go
+++ app.go
@@ -1,31 +1,41 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"os"
+
+// 	"github.com/gofiber/fiber/v2"
+// )
+
+// func main() {
+
+// 	port := "3000"
+// 	if os.Getenv("PORT") != "" {
+// 		port = os.Getenv("PORT")
+// 	}
+
+// 	app := fiber.New()
+
+// 	app.Get("/", func(c *fiber.Ctx) error {
+// 		return c.SendString("Hello, World!")
+// 	})
+// 	fmt.Println("Listening on port " + port)
+// 	app.Listen(port)
+// }
+
 package main
 
 import (
-	"embed"
-	"fmt"
-	"html/template"
-	"net/http"
-	"os"
-	"time"
+	"github.com/gofiber/fiber/v2"
 )
 
-//go:embed templates/*
-var templates embed.FS
-var t = template.Must(template.ParseFS(templates, "templates/*"))
-
-//go:embed static/*
-var assets embed.FS
-
 func main() {
-	port := "3000"
-	if os.Getenv("PORT") != "" {
-		port = os.Getenv("PORT")
-	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello " + r.RemoteAddr + "the time is\n " + time.Now().Format(time.RFC1123)))
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
 	})
 
-	fmt.Println("Listening on port " + port)
-	http.ListenAndServe(":"+port, nil)
+	app.Listen(":3000")
 }
